aa3077e550b7bdfaea3314b986391a09922e7dd7
H0udinii <hhaynes4@asu.edu>
2026-04-21 16:20:41 -0700

    updated js
    Going to try GSAP.timeline to animate page

--- main.js
+++ main.js
@@ -8,10 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
       end: "+=200%",
       pin: true,
       scrub: true,
-      markers: {
-    startColor: "yellow",
-    endcolor: "brown"
-  }
     },
     opacity: 0,
     ease: "power2.inOut"
@@ -29,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
       scrollTrigger: {
         trigger: "#fnaf_1",
         start: "top top",
-        end: "+=1000",
+        end: "bottom",
         scrub: 1,
         toggleActions: "play pause resume reset",
         anticipatePin: 1,
@@ -86,15 +82,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
       };
-//     gsap.from("#text1", {
-//   opacity: 0,
-//   y: 100,
-//   scrollTrigger: {
-//     trigger: "#fnaf_1",
-//     start: "bottom center",
-//     toggleActions: "play none none none"
-//   }
-// });
+    gsap.to(".text_1", {
+  scrollTrigger: {
+    trigger: "#fnaf_1",
+    start: "top",
+    end: "100px"
+  }
+});
     });
 
 
@@ -110,82 +104,127 @@ document.addEventListener("DOMContentLoaded", () => {
       0, 0, canvas.width, canvas.height
     );
   }
-// Freddy's Pizzeria //
-const {innerHeight} = window;
+// // Freddy's Pizzeria //
+// const {innerHeight} = window;
 
-gsap.to("#zoom-in", {
-  scale: 5, stagger: 0.50, duration:1,
-  ease: "none",
-  scrollTrigger:{
-    trigger: ".fnaf_2 h1",
-  start: "bottom+=250px top",
-  pin: true,
-  end: "+=800px",
-  scrub: true,
-  markers: {
-    startColor: "orange",
-    endcolor: "purple"
-  }
+// gsap.to("#zoom-in", {
+//   scale: 5, stagger: 0.50, duration:3,
+//   ease: "none",
+//   scrollTrigger:{
+//     trigger: "#fnaf_2 img",
+//   start: "bottom+=250px",
+//   end: "+=800px",
+//   pin: true,
+//   scrub: true,
+//   markers: {
+//     startColor: "orange",
+//     endcolor: "green"
+//   }
   
-  },
-  opacity: 0,
-    ease: "power2.inOut"
-});
+//   },
+//   opacity: 0,
+//     ease: "power2.inOut"
+// });
 
-// Abby Canvas //
+// // Abby Canvas //
 
-const canvas2 = document.getElementById("abby_canvas");
-  const ctx2 = canvas2.getContext("2d");
+// const canvas2 = document.getElementById("abby_canvas");
+//   const ctx2 = canvas2.getContext("2d");
 
-  let img2 = new Image();
-  let frames2 = [];
+//   let img2 = new Image();
+//   let frames2 = [];
 
-  fetch("./Assets/sprites/abby_stage_sprite.json")
-    .then(response => {
-      if (!response.ok) throw new Error("JSON not found");
-      return response.json();
-    })
-    .then(data => {
+//   fetch("./Assets/sprites/abby_stage_sprite.json")
+//     .then(response => {
+//       if (!response.ok) throw new Error("JSON not found");
+//       return response.json();
+//     })
+//     .then(data => {
 
-      frames2 = data.sprites;
+//       frames2 = data.sprites;
 
-      img2.src = "./Assets/sprites/abby_spritesheet.png";
+//       img2.src = "./Assets/sprites/abby_spritesheet.png";
+      
+//       img2.onload = () => {
+//         const state = {frame: 0};
 
-      img2.onload = () => {
-
-        const state = { frame: 0 };
-
-        gsap.from(state, {
-          frame: frames2.length - 1,
-          ease: "none",
-          scrollTrigger: {
-            trigger: ".abby_canvas",
-            start: "top 100px",
-            pin: true,
-            end: "+=200%",
-            scrub: true,
-            markers: {
-              startColor: "red",
-              endcolor: "green"
-  }
+//         gsap.to(state, {
+//           frame: frames2.length - 1,
+//           ease: "none",
+//           scrollTrigger: {
+//             trigger: ".panel2",
+//             start: "top 100px",    
+//             end: "+=200%",
+//             pin: true,
+//             scrub: true
             
-          },
-          onUpdate: () => {
-            drawAbbyFrame(Math.floor(state.frame));
-          }
-        });
-    };
-  });
-function drawAbbyFrame(index) {
-    const f = frames2[index];
-    if (!f) return;
+//           },
+          
+//           onUpdate: () => {
+//             drawAbbyFrame(Math.floor(state.frame));
+//           }
+//         });
+//     };
+//   });
+// function drawAbbyFrame(index) {
+//     const f = frames2[index];
+//     if (!f) return;
 
-    ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+//     ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
 
-    ctx2.drawImage(
-      img2,
-      f.x, f.y, f.width, f.height,
-      0, 0, canvas2.width, canvas2.height
-    );
-  }
+//     ctx2.drawImage(
+//       img2,
+//       f.x, f.y, f.width, f.height,
+//       0, 0, canvas2.width, canvas2.height
+//     );
+//   }
+  
+  
+//   // Puppet Canvas
+//   const canvas3 = document.getElementById("puppet_canvas");
+//   const ctx3 = canvas2.getContext("2d");
+
+//   let img3 = new Image();
+//   let frames3 = [];
+
+//   fetch("./Assets/sprites/puppet_sprite.json")
+//   .then(response => {
+//     if (!response.ok) throw new Error("JSON not found");
+//     return response.json();
+//   })
+//   .then(data => {
+    
+//     frames3 = data.sprites;
+    
+//     img3.src = "./Assets/sprites/puppet_spritesheet.png";
+//     img3.onload = () => {
+   
+//       const state = {frame: 0};
+
+//       gsap.to(state, {
+//         frame: frames3.length - 1,
+//           ease: "none",
+//           scrollTrigger: {
+//             trigger: ".panel3",
+//             start: "top 100px",    
+//             end: "+=200%",
+//             pin: true,
+//             scrub: true
+//       },
+      
+//       onUpdate: () => {
+//         drawPuppetFrame(Math.floor(state.frame));
+//       }
+//   });     
+//     };
+//   });
+//   function drawPuppetFrame(index){
+//     const f = frames3[index];
+//     if(!f) return;
+//     ctx3.clearRect(0, 0, canvas3.width, canvas3.height);
+
+//     ctx3.drawImage(
+//       img3, f.x, f.y, f.width, f.height, 0, 0, canvas3.width, canvas3.height
+//     );
+//   }
 });
